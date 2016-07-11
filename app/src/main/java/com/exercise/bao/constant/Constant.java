67b7e7a2e6d545a4329c5eba960b3bbbbc84e089
CrazyBunQnQ <baobao222222@qq.com>
2016-07-11 16:29:52 +0800

    调整结构

---
+++ app/src/main/java/com/exercise/bao/constant/Constant.java
@@ -0,0 +1,7 @@
+package com.exercise.bao.constant;
+
+/**
+ * Created by Administrator on 2016/7/11.
+ */
+public final class Constant {
+}
---
+++ app/src/main/java/com/exercise/bao/service/Downloader.java
@@ -0,0 +1,7 @@
+package com.exercise.bao.service;
+
+/**
+ * Created by Administrator on 2016/7/11.
+ */
+public class Downloader {
+}
