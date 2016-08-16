2565be935598e98ef90556093cf9cea95c3aee7c
CrazyBunQnQ <baobao222222@qq.com>
2016-08-17 01:11:00 +0800

    add some resources

--- app/src/main/java/com/exercise/bao/activities/HomeActivity.java
+++ app/src/main/java/com/exercise/bao/activities/HomeActivity.java
@@ -1,10 +1,24 @@
 package com.exercise.bao.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by CrazyBun on 2016/7/11.
  */
 public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
 
+    public static void lanuch(Context context) {
+        Intent intent = new Intent(context, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        context.startActivity(intent);
+    }
 }
--- app/src/main/java/com/exercise/bao/activities/MainActivity.java
+++ app/src/main/java/com/exercise/bao/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.exercise.bao.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -8,31 +9,35 @@ import com.exercise.bao.utils.*;
 
 public class MainActivity extends Activity {
 
+    private static final int OFFSET = 500;
+    private Context context;
+
+    public MainActivity() {
+        this.context = this;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TimeUtil.setCurrentTime();
-        TestCode();
-        Toast.makeText(this,"耗时" + TimeUtil.runTime() + "毫秒",Toast.LENGTH_LONG).show();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(OFFSET);
+                    HomeActivity.lanuch(context);
+                    finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+//        TimeUtil.setCurrentTime();
+//        TestCode();
+//        Toast.makeText(this,"耗时" + TimeUtil.runTime() + "毫秒",Toast.LENGTH_LONG).show();
     }
 
     private void TestCode() {
-        int[][][] aaa = new int[][][] {{{},{}},
-                {{1}, {1}},
-                {{0, 1, 0},{0}}};
-//                {0, 1, 0, 0, 1, 0, 3, 12},
-//                {0, 1, 0, 3, 0, 1, 0, 12},
-//                {0, 1, 0, 0, 1, 0, 0, 1, 0, 3, 12},
-//                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 5}};
-//        for (int i = 0; i < aaa.length; i++) {
-        int[] a = new int[] {4,5,7,8,9,12,19,21,22,23,35,40,41,51,60};
-        int[] b = new int[] {2,5,7,9,21,23,24,31,35,41, 51};
-        int[] c = ArrayUtil.merge(a, b);
-//        }
 
-        Toast.makeText(this,"结果：aaa = " + c.toString(),Toast.LENGTH_LONG).show();
-//        StringReverse.stringReverseSlow("")
     }
 }
